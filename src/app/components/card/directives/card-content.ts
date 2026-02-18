3091f43545636e18296b35034889cc1c664a5dce
Antonio Sitoe <antoniositoehl@gmail.com>
2026-02-18 10:21:05 +0200

    add directives

---
+++ src/app/components/card/directives/card-content.ts
@@ -0,0 +1,10 @@
+import { Directive } from '@angular/core';
+
+@Directive({
+  selector: '[appCardContent]',
+})
+export class CardContent {
+
+  constructor() { }
+
+}
---
+++ src/app/components/card/directives/card-header.ts
@@ -0,0 +1,9 @@
+import { Directive } from "@angular/core";
+
+@Directive({
+  selector: "app-card-header",
+  host: {
+    class: "card-header",
+  },
+})
+export class CardHeaderDirective {}
