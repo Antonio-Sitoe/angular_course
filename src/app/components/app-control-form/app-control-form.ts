0fc4db1191456d42ff1bdbd34335cfd4da80b0c5
Antonio Sitoe <antoniositoehl@gmail.com>
2026-03-27 10:08:05 +0200

    Ajustres

--- src/app/components/app-control-form/app-control-form.ts
+++ src/app/components/app-control-form/app-control-form.ts
@@ -1,16 +1,13 @@
-import { Component, OnInit } from "@angular/core";
+import { Component } from "@angular/core";
 import { FormControl, Validators } from "@angular/forms";
 
 @Component({
-  selector: "app-app-control-form",
   imports: [],
+  selector: "app-app-control-form",
   templateUrl: "./app-control-form.html",
-  styleUrl: "./app-control-form.css",
 })
 export class AppControlForm {
   nome = new FormControl("", [Validators.required, Validators.minLength(6)]);
 
-  alterarValor() {
-    this.nome.setValue("Novo valor");
-  }
+  alterarValor() {}
 }
