cb1c0e2b18d9790fcfc4cf6163b4a9333f61b4f9
Antonio Sitoe <antoniositoehl@gmail.com>
2026-04-17 13:49:52 +0200

    Register NgxMaskDirective in PhoneListEdit
    Import and register NgxMaskDirective in the PhoneListEdit component so the template can use input masking for phone fields. Adds the directive import from "ngx-mask" and includes it in the component's imports array.

--- src/app/components/contact-informations-edit/components/phone-list-edit/phone-list-edit.ts
+++ src/app/components/contact-informations-edit/components/phone-list-edit/phone-list-edit.ts
@@ -1,10 +1,11 @@
 import { Component } from "@angular/core";
 import { MatFormFieldModule } from "@angular/material/form-field";
 import { MatInputModule } from "@angular/material/input";
+import { NgxMaskDirective } from "ngx-mask";
 
 @Component({
   selector: "app-phone-list-edit",
-  imports: [MatFormFieldModule, MatInputModule],
+  imports: [MatFormFieldModule, MatInputModule, NgxMaskDirective],
   templateUrl: "./phone-list-edit.html",
 })
 export class PhoneListEdit {}
