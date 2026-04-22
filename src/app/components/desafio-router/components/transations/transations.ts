99c8f664198abac6a12dd28167744725d33f7a7e
Antonio Sitoe <antoniositoehl@gmail.com>
2026-04-22 11:03:18 +0200

    dsd

--- src/app/components/desafio-router/components/transations/transations.ts
+++ src/app/components/desafio-router/components/transations/transations.ts
@@ -1,9 +1,9 @@
 import { Component } from "@angular/core";
-import { RouterLink, RouterLinkActive, RouterOutlet } from "@angular/router";
+import { RouterOutlet } from "@angular/router";
 
 @Component({
   selector: "app-transations",
-  imports: [RouterOutlet, RouterLink, RouterLinkActive],
+  imports: [RouterOutlet],
   templateUrl: "./transations.html",
   styleUrl: "./transations.css",
 })
