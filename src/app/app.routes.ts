7f8d2da50bae2906aa0fd4752cce6e294e163175
Antonio Sitoe <antoniositoehl@gmail.com>
2026-04-27 12:28:21 +0200

    Remove app-router-guards component and clear routes configuration

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,4 +1,3 @@
 import { Routes } from "@angular/router";
-import { appRoutesGuards } from "./components/app-router-guards/app.routes";
 
-export const routes: Routes = [...appRoutesGuards];
+export const routes: Routes = [];
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,8 @@
 import { Component } from "@angular/core";
-import { AppRouterGuardsComponent } from "./components/app-router-guards/app-router-guards";
 
 @Component({
   selector: "app-root",
-  imports: [AppRouterGuardsComponent],
+  imports: [],
   templateUrl: "./app.html",
 })
 export class App {}
