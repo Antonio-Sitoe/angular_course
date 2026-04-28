5057a6744502602d17d1c334afa5f0c6194f8dcf
Antonio Sitoe <antoniositoehl@gmail.com>
2026-04-28 10:32:31 +0200

    Add app-http component with HTML and CSS files

---
+++ src/app/components/app-http/app-http.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-app-http',
+  imports: [],
+  templateUrl: './app-http.html',
+  styleUrl: './app-http.css',
+})
+export class AppHttp {
+
+}
